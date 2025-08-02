94da122a3cdb619e40fdb78c81c313c0d37cd693
Potitanem <ya.denis300000@yandex.ru>
2025-08-03 00:37:39 +0300

    update

--- contracts/Fund_me.sol
+++ contracts/Fund_me.sol
@@ -2,45 +2,38 @@
 
 pragma solidity ^0.8.24;
 
-interface AggregatorV3Interface {
-  function decimals() external view returns (uint8);
-
-  function description() external view returns (string memory);
-
-  function version() external view returns (uint256);
-
-  function getRoundData(
-    uint80 _roundId
-  ) external view returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
-
-  function latestRoundData()
-    external
-    view
-    returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
-}
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 contract FundMe{
-    
 
-    uint256 public minimumUsd = 5;
+
+    uint256 public minimumUsd = 5e18;
 
     function fund() public payable {
-        require(msg.value > minimumUsd, "Didn't send enough ETH");
+        require(getConversionRate(msg.value) > minimumUsd, "Didn't send enough ETH");
     }
 
-    function getPrice() public{
+    function getPrice() public view returns (uint256){
 
-        //Adress ETH/USD: 0x694AA1769357215DE4FAC081bf1f309aDC325306
-        //ABI
+        // Adress ETH/USD: 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        // ABI
+        AggregatorV3Interface dataFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        (,int256 answer,,,) = dataFeed.latestRoundData();
+        // Price of ETH in terms of USD
+        // 2000.00000000
+        return uint256(answer * 1e10);
+   }
 
-    }
-
-    function getConversionRate() public{
-
-    }
+    function getConversionRate(uint256 ethAmount) public view returns(uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+        return ethAmountInUsd;
+    } 
 
     function getVersion() public view returns (uint256){
         return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
     }
 
 }
+
+ 
